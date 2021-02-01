2ca80d5d503990ad9f14d80116884b3a10af5af9
moko256 <koutaro.mo@gmail.com>
2021-02-01 14:38:44 +0900

    chore: simplify generater script and add shebang

--- generate_tiles.bash
+++ generate_tiles.bash
@@ -1,10 +1,12 @@
+#!/usr/bin/bash
+
 pattern="!(track_*|island_40_*|island_33_multi_tile_01|island_3|seabed_island|ice_shelf_+([0-9])|*_instances|blank|*_test).xml"
 
 cd "/c/Program Files (x86)/Steam/steamapps/common/Stormworks/rom/data/tiles"
 shopt -s extglob
 ls $pattern
 tiles=$(ls $pattern | xargs -n1000 -d " ")
-locs=$(echo "$tiles" | awk '{print "			<l id=\""NR"\" tile=\"data/tiles/"$1"\" name=\"\" is_env_mod=\"false\" env_mod_spawn_num=\"1\"/>"}')
-cd - > /dev/null
+locs=$(echo "$tiles" | awk '{print "\t\t\t<l id=\""NR"\" tile=\"data/tiles/"$1"\" name=\"\" is_env_mod=\"false\" env_mod_spawn_num=\"1\"/>"}')
+cd -
 out_base=$(cat playlist_template.xml)
 printf "$out_base" $(($(echo -e $tiles | wc -w)+1)) "$locs" > playlist.xml
