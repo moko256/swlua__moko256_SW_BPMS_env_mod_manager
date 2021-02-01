6bc294361baae524855d8bd49af03a5595c4ed48
moko256 <koutaro.mo@gmail.com>
2021-02-01 13:39:05 +0900

    chore: reduce tiles

--- generate_tiles.bash
+++ generate_tiles.bash
@@ -1,7 +1,9 @@
+pattern="!(track_*|island_40_*|island_33_multi_tile_01|island_3|seabed_island|ice_shelf_+([0-9])|*_instances|blank|*_test).xml"
+
 cd "/c/Program Files (x86)/Steam/steamapps/common/Stormworks/rom/data/tiles"
 shopt -s extglob
-ls !(track_*|*_instances|blank|*_test).xml
-tiles=$(ls !(track_*|*_instances|blank|*_test).xml | xargs -n1000 -d " ")
+ls $pattern
+tiles=$(ls $pattern | xargs -n1000 -d " ")
 locs=$(echo "$tiles" | awk '{print "			<l id=\""NR"\" tile=\"data/tiles/"$1"\" name=\"\" is_env_mod=\"false\" env_mod_spawn_num=\"1\"/>"}')
 cd - > /dev/null
 out_base=$(cat playlist_template.xml)
